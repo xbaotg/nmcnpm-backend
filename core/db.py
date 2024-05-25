d26ca9383d1d2dacb8a11bbb7254e66d5f8692ac
xbaotg <supersoulbylt@gmail.com>
2024-05-26 00:31:50 +0700

    feat(db): update something

--- core/db.py
+++ core/db.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from core.config import config
-from sqlalchemy import text
 
 
 class Database:
